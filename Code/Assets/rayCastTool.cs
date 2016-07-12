34650ef42b9b90f91930c9f0ccb77cdccc4a391d
Jonathan43551 <Jonathan43551@gmail.com>
2016-07-11 20:14:18 -0700

    Initial
    Unity project for thesaurus4screens editor

---
+++ Code/Assets/rayCastTool.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+
+public class rayCastTool : MonoBehaviour
+{
+    public Rigidbody rayCastPoint_Particle;
+
+    public Rigidbody block;
+
+    public float speed = 50f;
+
+    public float spawnHeight = 10;
+
+
+
+
+
+  //  void SpawnBlock()
+  //  {
+  //
+  //
+  //      for (int y = 0; y < 1; y++)
+  //      {
+  //          // Rigidbody rocketClone = (Rigidbody)Instantiate(rocket, transform.position, transform.rotation);
+  //
+  //          Rigidbody rocketClone = (Rigidbody)Instantiate(block, new Vector3(y, spawnHeight, y), transform.rotation);
+  //
+  //          rocketClone.velocity = transform.forward * speed;
+  //
+  //
+  //      }
+  //
+  //
+  //  }
+  //
+
+    // Calls the fire method when holding down ctrl or mouse
+    void Update()
+    {
+    //    if (Input.GetMouseButton(0))
+    //    {
+    //        SpawnBlock();
+    //    }
+
+
+       if (Input.GetMouseButton(0))
+       {
+           Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+           RaycastHit hit;
+           if (Physics.Raycast(ray, out hit, 400))
+           {
+               //FireRocket();
+               Instantiate(rayCastPoint_Particle, hit.point, transform.rotation);
+           }
+    
+    
+       }
+    
+
+
+
+    }
+
+}
