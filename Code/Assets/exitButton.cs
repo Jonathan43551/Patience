a0a8205d4a159896faba1a93ee077b81ec5ac0cc
Jonathan43551 <Jonathan43551@gmail.com>
2016-10-05 23:01:37 -0700

    ship it

---
+++ Code/Assets/exitButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exitButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+        //docs.unity3d.com/ScriptReference/Application.Quit.html
+    }
+
+    public void quitPatience()
+    {
+        Application.Quit();
+    }
+    
+}
