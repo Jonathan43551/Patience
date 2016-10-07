9b2548b47e1ffdcb62438ed0d17e88b4e53f9b24
Jonathan43551 <Jonathan43551@gmail.com>
2016-10-06 22:13:51 -0700

    eventually figured out how lighting worked

---
+++ Code/Assets/activation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
