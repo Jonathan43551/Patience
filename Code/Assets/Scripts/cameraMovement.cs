a412cb874bdb32593ecdd2d5402f6faf2a299b68
Jonathan43551 <Jonathan43551@gmail.com>
2016-10-08 15:51:25 -0700

    Adding slider to control camera Y position

---
+++ Code/Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraMovement : MonoBehaviour {
+    //answers.unity3d.com/questions/824575/change-position-cameras-slider.html
+
+    public Camera mainCamera;
+
+    public float newYPosition;
+
+    void start()
+    {
+      float newYPosition = mainCamera.transform.localPosition.y;
+    }
+
+
+
+
+
+
+    // Update is called once per frame
+    public void SliderValueChanged(float sliderValue) {
+        newYPosition = newYPosition + sliderValue;
+        //Debug.Log("Y coordinate modifier: " + newYPosition);
+    }
+
+
+    public void Update()
+    {
+        mainCamera.transform.localPosition = new Vector3(
+        mainCamera.transform.localPosition.x,
+        newYPosition,
+        mainCamera.transform.localPosition.z
+        );
+
+        Debug.Log(mainCamera.transform.localPosition);
+    }
+
+}
+
