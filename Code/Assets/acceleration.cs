49bec96ce3e63c56f94a1845eeb100bcbba39dfa
Jonathan43551 <Jonathan43551@gmail.com>
2017-02-27 00:41:46 -0800

    paitenceKV_1.0.6 is available
    hooray, I removed the buttons, let's see how it looks :)

---
+++ Code/Assets/acceleration.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class acceleration : MonoBehaviour {
+
+    public Vector3 rotationPoint; 
+
+    void LateUpdate() {
+        
+        this.transform.RotateAround(rotationPoint,Vector3.up,0.1f);
+    }
+}
